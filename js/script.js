245050516f6c6ba86ee723b2419ff158d5dbfc05
vrossi96 <vrossi396@yahoo.it>
2022-01-17 13:47:18 +0100

    add what to do

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*
+L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+*/
