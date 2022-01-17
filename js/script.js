e697979f7d4d5548a6639c2a41d61698001feb88
vrossi96 <vrossi396@yahoo.it>
2022-01-17 15:06:19 +0100

    add classes and initial function for cells

--- js/script.js
+++ js/script.js
@@ -5,3 +5,23 @@ con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 */
+
+// crea funzione che crea le celle
+// La funzione prende il numero di celle e la difficoltà
+const createCell = (cellsNumber, difficulty) => {
+  const cell = document.createElement('div');
+	cell.className = `cell-${difficulty}`;
+	cell.id = cellsNumber;
+	cell.innerText = cellsNumber
+	return cell;
+}
+
+
+
+
+// ---------------------
+// Dati
+// ---------------------
+let row = 10;
+let column = 10;
+let totalCells = row * column;
